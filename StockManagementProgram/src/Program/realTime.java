247d6f66f28588abfaa09c3388dfe4f50142c6cb
Lee JIHO <vc54@naver.com>
2021-05-19 09:45:19 +0900

    파일 읽기까지 추가, 유저가 원할때 프로그램을 종료하는 기능은 연구가 필요

--- StockManagementProgram/src/Program/realTime.java
+++ StockManagementProgram/src/Program/realTime.java
@@ -1,14 +1,19 @@
 /**
  * Receive targetTime from user if targetTime and currentTime is same scan file stockData and print out list of lack of stock
  * @author LeeJiHo
- * @version alpha 0.1.0
- * @date 2021.05.15
+ * @version alpha 0.2.0
+ * @date 2021.05.19
  */
 package Program;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
@@ -49,11 +54,71 @@ public class realTime {
 	public void findLackStock()
 	{
 		LinkedList<String> list= new LinkedList<String>();
+		String line;
+		
+		try
+		{
+			File data = new File("Stocks.txt");
+			FileReader filereader = new FileReader(data);
+			BufferedReader buffer = new BufferedReader(filereader);
+			
+			while((line = buffer.readLine()) != null)
+			{
+				if(isOutOfStock(line))
+				{
+					list.add(line);
+				}				
+			}
+		}catch(FileNotFoundException e) {
+			
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		
+		
+	}
+	
+	//check stock data it is out of stock
+	public boolean isOutOfStock(String line)
+	{
+		String[ ] data = line.split("|");
+		
+		if(Integer.getInteger(data[1]) < 5)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public void waitForUserExit()
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Time checker is running if you stop please enter any integer number");
+		while(keyboard.hasNextInt() || this.isSameToTagetTime() == 1)
+		{
+			if(this.isSameToTagetTime() == 1)
+			{
+				System.out.println("Times up");
+				break;
+			}
+			else
+			{
+				System.out.println("End time counting");
+				break;
+			}
+		}
 	}
 	
 	public static void main(String args[])
 	{
-	
+		realTime test = new realTime();
+		
+		test.setTargetTime("09:04:30");
+		test.waitForUserExit();
 	}
 	
 }
