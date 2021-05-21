1e60469fb88fba0b1681d0bf0c41bd44ba829e53
Lee JIHO <vc54@naver.com>
2021-05-21 14:09:31 +0900

    통합

---
+++ StockManagementProgram/src/Program/stockReadymade.java
@@ -0,0 +1,29 @@
+package Program;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class stockReadymade {
+	private String name;//제품명
+	private String readyMadeCategory; //카테고리
+	private int amount;//수량
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return readyMadeCategory;
+	}
+	public void setCategory(String readyMadeCategory) {
+		this.readyMadeCategory = readyMadeCategory;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
