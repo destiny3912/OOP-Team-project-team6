252cea2b729cbda655066bc47029b04afda86cf1
dpwls228 <84014910+dpwls228@users.noreply.github.com>
2021-05-28 10:54:31 +0900

    Update Search.java
    add comment , modify txt file

--- StockManagementProgram/src/Program/Search.java
+++ StockManagementProgram/src/Program/Search.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Search {
-	private String findStock;
+	private String findStock; //input Name,number,Expiration date,expire
 	
 	public String getFindStock() {//getter
 		return findStock;
@@ -18,7 +18,7 @@ public class Search {
 		this.findStock = findStock;
 	}
 
-	boolean result;
+	boolean result; //To determine whether or not present.
 	
 	//Default Constructor
 	public Search() {
@@ -33,70 +33,71 @@ public class Search {
 	public void searchMethod() throws IOException
 	{
 		Scanner keyboard =new Scanner(System.in);
-		File file = new File("test.txt");
-		BufferedReader in = null;
+		File file = new File("Stocks.txt"); //file load
+		BufferedReader in = null; 
 		System.out.println("Enter stock to find:");
 		 
-		in = new BufferedReader( new InputStreamReader(System.in)); 
-		findStock = in.readLine(); //input word
+		in = new BufferedReader( new InputStreamReader(System.in));  //input sentence
+		findStock = in.readLine(); //sentence received store variable
+		String[] findStockName = findStock.split(","); //sentence split, exact name
 		    
-		in = new BufferedReader( new FileReader("test.txt"));
-		String line;
+		in = new BufferedReader( new FileReader("Stocks.txt")); //file read
+		String line; //txt line
 		
 		try {
-		while((line = in.readLine()) != null){ //file scan
-			result = false;
-		    String[] stockName = line.split(":"); //StockName:15
+		while((line = in.readLine()) != null){ //file scan until null
+			result = false; //Not find
+		    String[] stockName = line.split(","); //exact StockName in txt
 		     
-		     if(stockName[0].trim().compareToIgnoreCase(findStock.trim()) == 0){ //if success find
+		     if(stockName[0].trim().compareToIgnoreCase(findStockName[0].trim()) == 0){ //if success find
 		      System.out.println("Stock Name: " + stockName[0].trim());
 		      System.out.println("Stock Number: " + stockName[1]);
-		      result= true;
+		      result= true; //find
 		      break;
 		      }
 			}
 		}
 		catch (IOException e) {
 
-		        System.err.println(e); // ø°∑Ø∞° ¿÷¥Ÿ∏È ∏ﬁΩ√¡ˆ √‚∑¬
+		        System.err.println(e); // ÏóêÎü¨Í∞Ä ÏûàÎã§Î©¥ Î©îÏãúÏßÄ Ï∂úÎ†•
 
 		        System.exit(1);
 
 		    }
 		
-		if(result == false){
+		if(result == false){ //if fail to search stock 
 		    System.out.println("Fail to find " +findStock);
 		    System.out.println("Don't have " +findStock +" in stock");
 		    }
-		in.close();
+		in.close(); //file close
 	}
 	
 	public void printAllMethod() throws IOException{ //print all stock
 		Scanner keyboard =new Scanner(System.in);
-		File file = new File("test.txt");
+		File file = new File("Stocks.txt"); //file load
 		BufferedReader in = null;
 		
-		in = new BufferedReader( new FileReader("test.txt"));
-		String line;
+		in = new BufferedReader( new FileReader("Stocks.txt")); //file read
+		String line; //txt line
 		
 		try {
+		System.out.println("----Current number of Stock----");
 		while((line = in.readLine()) != null){ //file scan
-			System.out.println("----Current number of Stock----");
-		    System.out.println(line);
+		    System.out.println(line); //file output
 		    }
 			
 		}
 		catch (IOException e) {
 
-		        System.err.println(e); // ø°∑Ø∞° ¿÷¥Ÿ∏È ∏ﬁΩ√¡ˆ √‚∑¬
+		        System.err.println(e); // ÏóêÎü¨Í∞Ä ÏûàÎã§Î©¥ Î©îÏãúÏßÄ Ï∂úÎ†•
 
 		        System.exit(1);
 
 		    }
-		in.close();
+		in.close(); //file close
 	}
 
-public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException { //Temporary main 
 	Search test = new Search();
 	test.searchMethod(); 
 	test.printAllMethod();
